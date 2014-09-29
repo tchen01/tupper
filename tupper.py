1019c6472d771f1a5f1f9ddb06c9c596c45f3c67
Tyler Chen <tchen01@users.noreply.github.com>
2014-09-29 13:19:22 -0400

    Update tupper.py

--- tupper.py
+++ tupper.py
@@ -6,15 +6,15 @@ height = im.size[1]
 k = ''
 
 def color( c ):
-	sum = c[0] + c[1] + c[2]
-	if ( sum < 383 ):
-		return 1
-	else:
-		return 0
+  sum = c[0] + c[1] + c[2]
+  if ( sum < 383 ):
+    return 1
+  else:
+    return 0
 
 for x in range(0, width):
-	for y in reversed(range(0, height)):
-		k += str( color( im.getpixel( (x,y) ) ))
+  for y in reversed(range(0, height)):
+    k += str( color( im.getpixel( (x,y) ) ))
 
 k = int(k, base = 2) * height
 print("k=",k, "height=", height)
